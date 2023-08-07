bb3bdfd4e663e45b6b73519a6f4ee98fd21d3cb8
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-08-07 15:57:58 -0300

    feat: script to create the file that will self-document in the swagger, check the production environment and test and nodemon configuration

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors')
-const indexRouter = require('./routes/index');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger/swagger_output.json');
+const swaggerOptions = { customCssUrl: '/swagger-ui.css' };
+
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -16,7 +19,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
 app.use('/users', usersRouter);
+
+
+if (process.env.NODE_ENV !== 'test') {
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+}
 
 module.exports = app;
