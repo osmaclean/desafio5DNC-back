384aaa846acbbafa860524706abbc6c90f32f213
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-08-07 20:08:16 -0300

    feat: created swagger authentication middleware

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const swaggerUi = require('swagger-ui-express');
 const swaggerOptions = { customCssUrl: '/swagger-ui.css' };
 const routes = require('./src/routes');
+const authDocProduction = require('./src/middlewares/authDoc');
 const app = express();
 require('dotenv').config();
 
@@ -19,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 if (process.env.NODE_ENV !== 'test') {
   const swaggerFile = require('./swagger/swagger_output.json');
   app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
-  app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
+  app.use('/doc', /*authDocProduction*/ swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
 }
 
 routes(app);
