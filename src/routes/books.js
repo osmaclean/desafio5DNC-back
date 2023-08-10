e4a747ad6ce96d76d458bf1d15a4e1fcfa83f880
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-08-10 12:34:50 -0300

    feat: created book editing route

--- src/routes/books.js
+++ src/routes/books.js
@@ -4,7 +4,7 @@ const handleExpectedErrors = require('../functions/handleExpectedErrors');
 const EsquemaLivros = require('../models/livro');
 const router = express.Router();
 
-/* GET users listing. */
+
 router.post('/criar', connectBD, async function (req, res) {
   try {
     // #swagger.tags = ['Livros']
@@ -16,6 +16,32 @@ router.post('/criar', connectBD, async function (req, res) {
       statusMensagem: "Livro criado com sucesso",
       resposta: respostaBD
     })
+
+  } catch (error) {
+    return tratarErrosEsperados(res, error);
+  }
+});
+
+router.put('/editar/:id', connectBD, async function (req, res) {
+  try {
+    // #swagger.tags = ['Livros']
+    let idBook = req.params.id
+    let { titulo, paginas, isbn, editora } = req.body;
+
+    const checkBook = await EsquemaLivros.findOne({ _id: idBook });
+    if (!checkBook) throw new Error("Livro não encontrado")
+
+    const updateBooks = await EsquemaLivros.updateOne({ _id: idBook }, { titulo, paginas, isbn, editora });
+
+    if (updateBooks?.modifiedCount > 0) {
+      const dataBooks = await EsquemaLivros.findOne({ _id: idBook });
+
+      res.status(200).json({
+        status: "OK",
+        statusMensagem: "Livro atualizado com sucesso",
+        resposta: dataBooks
+      })
+    }
 
   } catch (error) {
     return tratarErrosEsperados(res, error);
